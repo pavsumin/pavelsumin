c0933c63124f616291b21644e62182fcbe4d17de
pavsumin <pavsumin@gmail.com>
2026-02-23 15:16:42 +0700

    WIP: Fix background trubles. v5. 1 universeback for desktop and another one for mobile

--- components/UniverseBackground.tsx
+++ components/UniverseBackground.tsx
@@ -1,17 +1,32 @@
 export default function UniverseBackground() {
 	return (
-		<div className='-top-[40px] -bottom-[40px] fixed inset-0 -z-50 pointer-events-none overflow-hidden'>
-			{/* Base color */}
-			<div className='absolute inset-0 bg-[#f8fafc]' />
+		<>
+			{/* ===== DESKTOP VERSION (md+) ===== */}
+			<div className='hidden md:block fixed inset-0 -z-50 pointer-events-none overflow-hidden'>
+				{/* base */}
+				<div className='absolute inset-0 bg-[#f8fafc]' />
 
-			{/* bottom-left glow */}
-			<div className='absolute w-[900px] h-[900px] -bottom-[300px] -left-[300px] bg-accent/20 rounded-full blur-[220px]' />
+				{/* bottom-left glow */}
+				<div className='absolute w-[900px] h-[900px] -bottom-[300px] -left-[300px] bg-accent/20 rounded-full blur-[220px]' />
 
-			{/* top-right glow */}
-			<div className='absolute w-[700px] h-[700px] -top-[200px] -right-[200px] bg-blue-400/15 rounded-full blur-[200px]' />
+				{/* top-right glow */}
+				<div className='absolute w-[700px] h-[700px] -top-[200px] -right-[200px] bg-blue-400/15 rounded-full blur-[200px]' />
 
-			{/* center glow */}
-			<div className='absolute w-[600px] h-[600px] top-[30%] left-[40%] bg-purple-400/10 rounded-full blur-[180px]' />
-		</div>
+				{/* center glow */}
+				<div className='absolute w-[600px] h-[600px] top-[30%] left-[40%] bg-purple-400/10 rounded-full blur-[180px]' />
+			</div>
+
+			{/* ===== MOBILE VERSION (< md) ===== */}
+			<div className='block md:hidden absolute inset-0 -z-50 pointer-events-none overflow-hidden'>
+				{/* base */}
+				<div className='absolute inset-0 bg-[#f8fafc]' />
+
+				{/* softer centered glow */}
+				<div className='absolute w-[700px] h-[700px] top-[5%] left-1/2 -translate-x-1/2 bg-accent/15 rounded-full blur-[180px]' />
+
+				{/* subtle secondary glow */}
+				<div className='absolute w-[500px] h-[500px] top-[50%] left-1/2 -translate-x-1/2 bg-blue-400/10 rounded-full blur-[160px]' />
+			</div>
+		</>
 	)
 }
