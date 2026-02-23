fa0a80793dd4e3890a4d3657d08d744afda17c19
pavsumin <pavsumin@gmail.com>
2026-02-23 14:02:54 +0700

    Sitemap update

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,8 +1,30 @@
-export default function sitemap() {
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
 	return [
 		{
 			url: 'https://pavelsumin.com',
 			lastModified: new Date(),
+			changeFrequency: 'weekly',
+			priority: 1,
+		},
+		{
+			url: 'https://pavelsumin.com/#about',
+			lastModified: new Date(),
+			changeFrequency: 'monthly',
+			priority: 0.8,
+		},
+		{
+			url: 'https://pavelsumin.com/#work',
+			lastModified: new Date(),
+			changeFrequency: 'monthly',
+			priority: 0.9,
+		},
+		{
+			url: 'https://pavelsumin.com/#contact',
+			lastModified: new Date(),
+			changeFrequency: 'monthly',
+			priority: 0.7,
 		},
 	]
 }
