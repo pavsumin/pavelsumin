6c46a231d09c0bf5377311c93a3119533c176f58
pavsumin <pavsumin@gmail.com>
2026-02-25 20:05:04 +0700

    Mobile blur gradient back. (UniverseBackground)

--- components/UniverseBackground.tsx
+++ components/UniverseBackground.tsx
@@ -1,17 +1,37 @@
 export default function UniverseBackground() {
 	return (
-		<div className='hidden sm:block fixed inset-0 -z-50 pointer-events-none overflow-hidden'>
-			{/* Base color */}
-			<div className='absolute inset-0 bg-[#f8fafc]' />
+		<>
+			{/* Mobile subtle glow */}
+			<div className='fixed inset-0 -z-50 pointer-events-none sm:hidden'>
+				{/* чистый фон */}
+				<div className='absolute inset-0 bg-[#f8fafc]' />
 
-			{/* bottom-left glow */}
-			<div className='absolute w-[900px] h-[900px] -bottom-[300px] -left-[300px] bg-accent/20 rounded-full blur-[220px]' />
+				{/* мягкий центральный glow */}
+				<div
+					className='
+          absolute
+          top-[15%]
+          left-1/2
+          -translate-x-1/2
+          w-[500px]
+          h-[500px]
+          bg-blue-400/20
+          rounded-full
+          blur-[120px]
+        '
+				/>
+			</div>
 
-			{/* top-right glow */}
-			<div className='absolute w-[700px] h-[700px] -top-[200px] -right-[200px] bg-blue-400/15 rounded-full blur-[200px]' />
+			{/* Desktop full universe */}
+			<div className='hidden sm:block fixed top-[10px] bottom-[10px] left-0 right-0 -z-50 pointer-events-none overflow-hidden'>
+				<div className='absolute inset-0 bg-[#f8fafc]' />
 
-			{/* center glow */}
-			<div className='absolute w-[600px] h-[600px] top-[30%] left-[40%] bg-purple-400/10 rounded-full blur-[180px]' />
-		</div>
+				<div className='absolute w-[900px] h-[900px] -bottom-[300px] -left-[300px] bg-accent/20 rounded-full blur-[220px]' />
+
+				<div className='absolute w-[700px] h-[700px] -top-[200px] -right-[200px] bg-blue-400/15 rounded-full blur-[200px]' />
+
+				<div className='absolute w-[600px] h-[600px] top-[30%] left-[40%] bg-purple-400/10 rounded-full blur-[180px]' />
+			</div>
+		</>
 	)
 }
